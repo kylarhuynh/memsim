e35047705f0bf6b66e8ebd9c5c90a61dde43a6e4
Kylar Huynh <102556887+kylarhuynh@users.noreply.github.com>
2024-09-24 20:07:50 -0700

    Update proj2memsim.c

--- proj2memsim.c
+++ proj2memsim.c
@@ -10,7 +10,7 @@
 #include<sys/wait.h>
 
 /*define the pagesize*/
-#define PAGESIZE 4096;
+#define PAGESIZE 4096
 
 int init = 0;
 void *startofheap = NULL;
@@ -18,11 +18,11 @@ void *startofheap = NULL;
 typedef unsigned char BYTE;
 typedef struct chunkhead{
     unsigned int size;
-    unsigned int info; /*0 if it is free, 1 if it is not free*/
-    unsigned char *next,*prev;
-}chunkhead;
+    unsigned int info; /* 0 if it is free, 1 if it is not free */
+    unsigned char *next, *prev;
+} chunkhead;
 
-/*function declarations*/
+/* Function declarations */
 unsigned char *mymalloc(unsigned int);
 void myfree(unsigned char*);
 void analyze();
@@ -31,11 +31,8 @@ chunkhead *chunkhead0;
 chunkhead *current;
 
 int main(int argc, char const *argv[]){
-    clock_t start;
-    clock_t end;
-    double timediff;
-    int i;
-    unsigned char *a,*b,*c,*d,*e;
+    unsigned char *a, *b, *c, *d, *e;
+    
     analyze();
     a = mymalloc(4000);
     b = mymalloc(4000);
@@ -45,39 +42,24 @@ int main(int argc, char const *argv[]){
     myfree(e);
     myfree(d);
     analyze();
-
-    // BYTE *a[100];
-    // start = clock();
-    // analyze();/*50% points*/
-    // for(i=0;i<100;i++)
-    // a[i]= mymalloc(1000);
-    // for(i=0;i<90;i++)
-    // myfree(a[i]);
-    // analyze(); /*50% of points if this is correct*/
-    // myfree(a[95]);
-    // a[95] = mymalloc(1000);
-    // analyze();/*25% points, this new chunk should fill the smaller free one*/
-    // /*(best fit)*/
-    // for(i=90;i<100;i++)
-    // myfree(a[i]);
-    // analyze();/* 25% should be an empty heap now with the start address */
-    // /*from the program start*/
-    // end = clock();
-    // timediff = (double)(end - start) / CLOCKS_PER_SEC;
-    // printf("Result Time: %f seconds\n", timediff);
-    // return 0;
+    
+    return 0;
 }
 
-unsigned char *mymalloc(unsigned int size){
+unsigned char *mymalloc(unsigned int size) {
     chunkhead *last;
     chunkhead *new;
     chunkhead *bestfit = NULL;
     void *newptr = NULL;
-    if(size > 0){
-        if(size % 4096 !=  0){
-            size = size + (4096 - (size % 4096));
+
+    /* Adjust size to page size boundary */
+    if (size > 0) {
+        if (size % PAGESIZE != 0) {
+            size += (PAGESIZE - (size % PAGESIZE));
         }
-        if(init == 0){
+
+        /* If first time allocation */
+        if (init == 0) {
             startofheap = sbrk(size);
             chunkhead0 = (chunkhead *)startofheap;
             chunkhead0->size = size;
@@ -85,32 +67,31 @@ unsigned char *mymalloc(unsigned int size){
             chunkhead0->next = NULL;
             chunkhead0->prev = NULL;
             init += 1;
-            return startofheap;
-        }
-        else{
+            return (unsigned char *)(chunkhead0 + 1); /* Return memory after the chunkhead */
+        } 
+        else {
             current = chunkhead0;
-            while(current != NULL){
-                if(current->next == NULL){
+            while (current != NULL) {
+                /* Track the last chunk */
+                if (current->next == NULL) {
                     last = current;
                 }
-                if(current-> info == 0 && current->size == size){
+                /* Perfect fit */
+                if (current->info == 0 && current->size == size) {
                     current->info = 1;
-                    return (unsigned char *)current;
+                    return (unsigned char *)(current + 1);
                 }
-                if(current->info == 0 && current->size >= size){
-                    /* here is where i find best fit */
-                    if(bestfit == NULL){
+                /* Find best fit chunk */
+                if (current->info == 0 && current->size >= size) {
+                    if (bestfit == NULL || current->size < bestfit->size) {
                         bestfit = current;
                     }
-                    else{
-                        if(current->size < bestfit->size){
-                            bestfit = current;
-                        }
-                    }    
                 }
-            current = (chunkhead*)current->next;
+                current = (chunkhead *)current->next;
             }
-            if(bestfit == NULL){
+
+            /* No best fit found, allocate new chunk */
+            if (bestfit == NULL) {
                 current = last;
                 newptr = sbrk(size);
                 new = (chunkhead *)newptr;
@@ -118,61 +99,64 @@ unsigned char *mymalloc(unsigned int size){
                 new->info = 1;
                 new->next = current->next;
                 new->prev = (unsigned char *)current;
-                current->next = (unsigned char*)new;
-                return newptr;
-            }
-            else{ /*split memory*/
-                new = (chunkhead *)((unsigned char *)bestfit + size);
-                new->size = bestfit->size - size;   
-                new->info = 0;
-                new->next = bestfit->next;             
-                new->prev = (unsigned char *)bestfit;
+                current->next = (unsigned char *)new;
+                return (unsigned char *)(new + 1); /* Return memory after the chunkhead */
+            } 
+            else {
+                /* Split the best-fit chunk */
+                if (bestfit->size > size + sizeof(chunkhead)) {
+                    new = (chunkhead *)((unsigned char *)bestfit + sizeof(chunkhead) + size);
+                    new->size = bestfit->size - size - sizeof(chunkhead);
+                    new->info = 0;
+                    new->next = bestfit->next;
+                    new->prev = (unsigned char *)bestfit;
 
-                if(bestfit->next != NULL){
-                    ((chunkhead *)bestfit->next)->prev = (unsigned char *)new;
+                    if (bestfit->next != NULL) {
+                        ((chunkhead *)bestfit->next)->prev = (unsigned char *)new;
+                    }
+
+                    bestfit->next = (unsigned char *)new;
+                    bestfit->size = size;
                 }
-
-                bestfit->size = size;
                 bestfit->info = 1;
-                bestfit->next = (unsigned char *)new;
-                return (unsigned char *)bestfit;
+                return (unsigned char *)(bestfit + 1);
             }
         }
     }
+    return NULL; /* In case size is 0 or invalid */
 }
 
-void myfree(unsigned char *address){
-    current = (chunkhead *)(address);
+void myfree(unsigned char *address) {
+    current = (chunkhead *)(address - sizeof(chunkhead)); /* Go back to chunkhead */
     current->info = 0;
 
-    /*merge with previous chunk*/
-    if(current->prev != NULL && ((chunkhead *)current->prev)->info == 0){
-        ((chunkhead *)current->prev)->size = ((chunkhead *)current->prev)->size + current->size;
+    /* Merge with previous chunk if free */
+    if (current->prev != NULL && ((chunkhead *)current->prev)->info == 0) {
+        ((chunkhead *)current->prev)->size += current->size + sizeof(chunkhead);
         ((chunkhead *)current->prev)->next = current->next;
-        if(current->next != NULL){
+        if (current->next != NULL) {
             ((chunkhead *)current->next)->prev = current->prev;
         }
-        current = ((chunkhead *)current->prev);
+        current = (chunkhead *)current->prev;
     }
 
-    /*merge with next chunk*/
-    if(current->next != NULL && ((chunkhead *)current->next)->info == 0){
-        current->size = ((chunkhead *)current->next)->size + current->size;
+    /* Merge with next chunk if free */
+    if (current->next != NULL && ((chunkhead *)current->next)->info == 0) {
+        current->size += ((chunkhead *)current->next)->size + sizeof(chunkhead);
         current->next = ((chunkhead *)current->next)->next;
-        if(current->next != NULL){
+        if (current->next != NULL) {
             ((chunkhead *)current->next)->prev = (unsigned char *)current;
         }
-    }    
+    }
 
-    /*move back program break when last chunk freed*/
-    if(current->next == NULL){
-        if(current->prev != NULL){
+    /* Move program break back if last chunk */
+    if (current->next == NULL) {
+        if (current->prev != NULL) {
             current = (chunkhead *)current->prev;
-            sbrk(-(((chunkhead *)current->next)->size));
+            sbrk(-(current->size + sizeof(chunkhead)));
             current->next = NULL;
-        }
-        else{
-            sbrk(-(current->size));
+        } else {
+            sbrk(-(current->size + sizeof(chunkhead)));
             chunkhead0 = NULL;
             startofheap = NULL;
             init = 0;
@@ -180,22 +164,24 @@ void myfree(unsigned char *address){
     }
 }
 
-void analyze(){
+void analyze() {
     int no;
-    chunkhead* ch;
+    chunkhead *ch;
+
     printf("\n--------------------------------------------------------------\n");
-    if(!startofheap){
-        printf("no heap, program break on address: %x\n",sbrk(0));
+    if (!startofheap) {
+        printf("No heap, program break at: %p\n", sbrk(0));
         return;
     }
-    ch = (chunkhead*)startofheap;
-    for (no=0; ch; ch = (chunkhead*)ch->next,no++){
-        printf("%d | current addr: %x |", no, ch);
+
+    ch = (chunkhead *)startofheap;
+    for (no = 0; ch; ch = (chunkhead *)ch->next, no++) {
+        printf("%d | current addr: %p | ", no, (void *)ch);
         printf("size: %d | ", ch->size);
         printf("info: %d | ", ch->info);
-        printf("next: %x | ", ch->next);
-        printf("prev: %x", ch->prev);
-        printf(" \n");
+        printf("next: %p | ", (void *)ch->next);
+        printf("prev: %p", (void *)ch->prev);
+        printf("\n");
     }
-    printf("program break on address: %x\n",sbrk(0));
+    printf("Program break at: %p\n", sbrk(0));
 }
